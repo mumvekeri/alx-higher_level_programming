0417f4fe7935a6ba367f42ba1e496ceb5e47c443
mumvekeri <mumvekerichidochashe@gmail.com>
2023-10-05 04:20:00 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-# Assign the values to a and b
-a = 1
-b = 2
-
-# Print the result of the addition
-print(f"{a} + {b} = {add(a, b)}")
-
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
