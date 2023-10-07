692dcd2610ae21050fa9d5b62c0b994f1ab4ed5a
mumvekeri <mumvekerichidochashe@gmail.com>
2023-10-07 03:53:38 -0700

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - Checks if a simgly List is a palindrome
+ * @head: Double pointer
+ * Return: 0 if not else 1
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *ptr;
+	int array[3000] = {0};
+	int len = 0, r_idx = 2999;
+
+	if (head == NULL)
+		return (1);
+
+	ptr = *head;
+
+	if (ptr == NULL || ptr->next == NULL)
+		return (1);
+
+	while (ptr != NULL)
+	{
+		array[r_idx] = ptr->n;
+		len++;
+		r_idx--;
+		ptr = ptr->next;
+	}
+	ptr = *head;
+	r_idx++;
+
+	while (ptr != NULL)
+	{
+		if (ptr->n != array[r_idx])
+			return (0);
+		r_idx++;
+		ptr = ptr->next;
+	}
+
+	return (1);
+}
