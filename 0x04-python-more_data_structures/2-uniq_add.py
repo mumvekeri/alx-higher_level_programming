462649eccc2de88349a31deba6b76d4b083c51d6
mumvekeri <mumvekerichidochashe@gmail.com>
2023-10-11 07:03:39 -0700

    dicts

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_set = set()
-    total = 0
-
-    for number in my_list:
-        if number not in unique_set:
-            total += number
-            unique_set.add(number)
-
-    return total
-
-# Example usage:
-my_list = [1, 2, 2, 3, 3, 4, 5, 5, 5]
-result = uniq_add(my_list)
-print(result)
-
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
