27d576fcdb102c6b6c3d0ff2c618b725b04a5ae0
mumvekeri <mumvekerichidochashe@gmail.com>
2023-10-23 05:08:07 -0700

    try

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    # use a try-except block to handle possible errors
     try:
-        # print the value as an integer, followed by a new line
         print("{:d}".format(value))
-        # return True if no error occurs
         return True
-    except:
-        # return False if an error occurs
+    except (ValueError, TypeError):
         return False
