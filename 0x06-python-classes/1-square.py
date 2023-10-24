496dba85f2d505c553f85a633ca90644e2ecefd8
mumvekeri <mumvekerichidochashe@gmail.com>
2023-10-24 02:22:19 -0700

    squares

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """A class that represents a square."""
+
+    def _init_(self, size):
+        """Initialize the square with the given size."""
+        self.__size = size # private instance attribute
