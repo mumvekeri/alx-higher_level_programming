acacfc01ed2f724d9b4b2b8e158f89a0910d5c85
mumvekeri <mumvekerichidochashe@gmail.com>
2023-12-12 09:47:19 -0800

    try

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,9 +1,2 @@
 -- A script that lists all the tables of a database in your MySQL server.
--- Check for argument presence
-IF @@parameter_count < 1 THEN
-  SELECT 'Please provide a database name as an argument.';
-ELSE
-  SELECT TABLE_NAME
-  FROM information_schema.tables
-  WHERE TABLE_SCHEMA = @`{1}`;
-END IF;
+SHOW TABLES;
