12dd7997ac74a1b040099f4ae80fe0ed12c46558
mumvekeri <mumvekerichidochashe@gmail.com>
2024-01-25 01:26:10 -0800

    try

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a GET request with a custom header
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# a Bash script that takes in a URL as an argument, sends a GET request to the
+#URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
