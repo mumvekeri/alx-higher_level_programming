84a4658bdc40655503de6731517cce80d451d5eb
mumvekeri <mumvekerichidochashe@gmail.com>
2024-01-26 03:29:41 -0800

    try

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""a Python script that fetches https://alx-intranet.hbtn.io/status """
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urllib package
+"""
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-        print("- Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-except urllib.error.HTTPError as e:
-    print("Error fetching the URL:", e)
-except urllib.error.URLError as e:
-    print("URL error:", e)
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+            print("- Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except urllib.error.HTTPError as e:
+        print("Error fetching the URL:", e)
+    except urllib.error.URLError as e:
+        print("URL error:", e)
