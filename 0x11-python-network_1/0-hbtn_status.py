4093b443ae123dc54e7a83f20fd4d1c90b590d1c
mumvekeri <mumvekerichidochashe@gmail.com>
2024-01-26 03:32:41 -0800

    try

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """A script that
 - fetches https://alx-intranet.hbtn.io/status.
-- uses urllib package
+- uses urlib package
 """
 
 
-import urllib.request
-
 if __name__ == '__main__':
-    url = "https://alx-intranet.hbtn.io/status"
+    import urllib.request
 
-    try:
-        with urllib.request.urlopen(url) as response:
-            content = response.read()
-            print("- Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode('utf-8')))
-    except urllib.error.HTTPError as e:
-        print("Error fetching the URL:", e)
-    except urllib.error.URLError as e:
-        print("URL error:", e)
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
