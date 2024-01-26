44bab64cff06f31f8a426af4c28b95a1f46aeff6
mumvekeri <mumvekerichidochashe@gmail.com>
2024-01-26 04:51:58 -0800

    try

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+''' GET request to https://intranet.hbtn.io/status using requests module '''
 import requests
 
-
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+if __name__ == '__main__':
+    res = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(res.text)))
+    print('\t- content: {}'.format(res.text))
