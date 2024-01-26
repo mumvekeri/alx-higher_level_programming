32eb6961f0ea6398b1b82cc2a2e7d105de1306a3
mumvekeri <mumvekerichidochashe@gmail.com>
2024-01-26 03:37:29 -0800

    try

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-import urllib.request
+"""A script that, takes in a URL, sends a request to the URL
+-and displays the value of the X-Request-Id variable found
+-in the header ofthe response.
+"""
 import sys
+import urllib.request
 
-# Check if a URL is provided as a command-line argument
-if len(sys.argv) < 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-url = sys.argv[1]
-
-try:
-    with urllib.request.urlopen(url) as response:
-        # Check if the 'X-Request-Id' header is present in the response
-        if 'X-Request-Id' in response.headers:
-            request_id = response.headers['X-Request-Id']
-            print("X-Request-Id:", request_id)
-        else:
-            print("X-Request-Id not found in the response headers.")
-except urllib.error.HTTPError as e:
-    print("HTTP Error:", e)
-except urllib.error.URLError as e:
-    print("URL Error:", e)
-except Exception as e:
-    print("An unexpected error occurred:", e)
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
