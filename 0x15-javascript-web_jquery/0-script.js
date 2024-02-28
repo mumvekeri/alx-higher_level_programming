bc14edfeb6550464adc09d70c2f4a0b6b44462ba
mumvekeri <mumvekerichidochashe@gmail.com>
2024-02-28 06:50:31 -0800

    try

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// JavaScript code to change the text color of the <header> element to red
+document.addEventListener('DOMContentLoaded', function () {
+var headerElement = document.querySelector('header');
+headerElement.style.color = '#FF0000';
